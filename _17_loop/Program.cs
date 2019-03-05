9b51ed520652b2fbdc71172805b60ad7ca94dad5
daking0 <48229706+daking0@users.noreply.github.com>
2019-03-05 11:00:03 +0900

    first

---
+++ _17_loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num); //++num은 즉시 1을 증가, num++ 일단 사용하고 나서 1을 증가
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+        }
+    }
+}
---
+++ _18_while/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* While문 :()안의 내용이 참인 조건일 때 계속 실행문을 반복
+ * while(참)
+   {
+       실행문
+    } 
+*/
+namespace _18_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0; //시작 조건
+            while (num < 1000) //종료 조건
+            {
+                //실행문
+                Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+            }
+            /*
+             while (num < 10)
+                Console.WriteLine("나무를 {0}번 찍었습니다.", ++num);
+             */
+        }
+    }
+}
