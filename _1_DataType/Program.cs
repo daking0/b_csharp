318a208911586ce5cbb40819b89483335e306cf1
daking0 <48229706+daking0@users.noreply.github.com>
2019-03-05 10:43:14 +0900

    first

---
+++ _1_DataType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            double dnum = 3.14;
+            char ch = 'a';
+            string name = "홍길동";
+            Console.WriteLine(num);
+            Console.WriteLine(dnum);
+            Console.WriteLine(ch);
+            Console.WriteLine(name);
+        }
+    }
+}
