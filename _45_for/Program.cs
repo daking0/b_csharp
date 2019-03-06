af47fa26043b31652ce4e75e57438c698ef5ca81
daking0 <48229706+daking0@users.noreply.github.com>
2019-03-06 14:01:09 +0900

    first

---
+++ _45_for/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+입력받은 숫자의 구구단을 출력하는데 역순으로 출력
+  예) 3*9=27
+      3*8=24... 
+*/
+namespace _45_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 0;
+            string num0 = "";
+
+            Console.WriteLine("숫자를 입력하세요.");
+            num0 = Console.ReadLine();
+            num1 = Int32.Parse(num0);
+
+            for (int k = 9; k >0; k--)
+            {
+                Console.WriteLine("{0} X {1} = {2}", num1, k, num1 * k);
+            }
+        }
+    }
+}
---
+++ _46_for/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ 정수 n을 입력받고 n!을 구하세요.(factorial)
+ 5를 입력한경우 1*2*3*4*5를 구하세요.
+*/
+namespace _46_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, result =1;
+            string num0 = "";
+
+            Console.WriteLine("숫자를 입력하세요.");
+            num0 = Console.ReadLine();
+            num1 = Int32.Parse(num0);
+
+            for(int i = 1; i <= num1; i++)
+            {
+                result = result * i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
