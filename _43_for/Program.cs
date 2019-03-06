10351fdd130e2412999d05f001f6dfc2b758cc13
daking0 <48229706+daking0@users.noreply.github.com>
2019-03-06 14:00:28 +0900

    first

---
+++ _43_for/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ 0부터 1씩 증가한 값을 누적하여 더합니다.
+ 모두 더한값이 1000이 넘을 때 의 총합과 
+ 더한 값을 구하세요.
+ */
+namespace _43_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int result = 0;
+
+            for (int i = 0; i >= 0; i++)
+            {
+                result += i;
+                if (result >= 1000)
+                    break;
+            }
+
+            //while (true)
+            //{
+            //    num1++;
+            //    result = num1 + result;
+            //    if (result >= 1000)
+            //        break;
+            //}
+            Console.WriteLine("1000이 넘을 때의 값은 " + result);
+        }
+    }
+}
---
+++ _44_for/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//두 수를 입력받고 최소공배수를 구하세요.
+namespace _44_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 0;
+            string num0 = "";
+
+            Console.WriteLine("숫자를 입력하세요.");
+            num0 = Console.ReadLine();
+            num1 = Int32.Parse(num0);
+
+            Console.WriteLine("숫자를 입력하세요.");
+            num0 = Console.ReadLine();
+            num2 = Int32.Parse(num0);
+
+            if (num1 >= num2)
+            {
+                if (num1 % num2 == 0)
+                {
+                    Console.WriteLine("최소공배수는 " + num1);
+                }
+                else
+                {
+                    Console.WriteLine("최소공배수는 " + (num1 * num2));
+                }
+            }
+            else
+            {
+                if (num2 % num1 == 0)
+                {
+                    Console.WriteLine("최소공배수는 " + num2);
+                }
+                else
+                {
+                    Console.WriteLine("최소공배수는 " + (num1 * num2));
+                }
+            }
+
+
+        }
+    }
+}
