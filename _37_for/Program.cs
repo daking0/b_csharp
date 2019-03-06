83dbcfd47407a5516b08744f088f1e514c43a2aa
daking0 <48229706+daking0@users.noreply.github.com>
2019-03-06 13:58:37 +0900

    first

---
+++ _37_for/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//문제-for문을 이용해서 10부터 20까지 출력하세요
+namespace _37_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =10;i<21;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ _39_for/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ for문을 이용해서 1부터 10까지 중에 짝수만 출력하세요
+ for문내에 조건문을 넣어주면 됨
+ 짝수 조건 if(num % 2 == 0)
+ */
+namespace _39_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<11;i++)
+            {
+                if(i %2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
